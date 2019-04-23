516bfe168853163865fe9eb66cb9ae71c029b7cf
Ivan Viveros <ivan.viveros@correounivalle.edu.co>
2019-04-23 18:55:18 -0500

    Actualización versión portable de R

--- runR.bash
+++ runR.bash
@@ -2,15 +2,17 @@
 # extraer R
 d=$(date +'%G/%m/%d %H:%M:%S')
 h=$(hostname -f)
-echo "${d} inicia extraccion r330.tgz en ${h}"
-tar xfz r330.tgz
+echo "${d} inicia extraccion portableR.tgz en ${h}"
+tar xfz portableR.tgz
 d=$(date +'%G/%m/%d %H:%M:%S')
-echo "${d} finaliza extraccion r330.tgz"
+echo "${d} finaliza extraccion portableR.tgz"
 
 # Ejecutar R con el archivo indicado
 d=$(date +'%G/%m/%d %H:%M:%S')
 echo "${d} inicia ejecución r con entradas $1"
-export R_HOME="${PWD}/R-3.3.0"
-R-3.3.0/bin/R -q -f $1
+export LD_LIBRARY_PATH=`pwd`/portableR/lib
+export R_HOME=`pwd`/portableR
+export R_LIBS=`pwd`/portableR/site-library
+./portableR/R -q -f $1
 d=$(date +'%G/%m/%d %H:%M:%S')
 echo "${d} finaliza ejecución r con entradas $1"
